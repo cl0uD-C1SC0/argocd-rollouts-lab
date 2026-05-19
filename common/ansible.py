edb88f6e33963f48d9ebf7106bf28f082ac18a33
cl0uD-C1SC0 <craftgamer807@gmail.com>
2026-05-19 16:10:42 -0300

    feat: improved install cli function and reduced complexity of general scripts function

--- common/ansible.py
+++ common/ansible.py
@@ -5,7 +5,11 @@ from common.util import run_command
 
 
 def check_required_cli_commands(package):
-    return shutil.which(package) is not None
+    tool = shutil.which(package) is not None
+    if tool:
+        print(f" ✅  The CLI: {package} was found on the system!")
+        return True
+    return False
 
 def get_argocd_credentials(BASE_PATH):
     print(" ℹ️  Getting Argo credentials")
@@ -15,35 +19,27 @@ def get_argocd_credentials(BASE_PATH):
             if "ARGO" in line.replace(' ', ''):
                 file.write(line.strip().replace('"', '').replace(",", "") +"\n")
     
-    print(" ✅ Credentials are been saved on the following file: argocd_credentials.txt")
+    print(" ✅  Credentials are been saved on the following file: argocd_credentials.txt")
 
 def install_cli_scripts(INVENTORY_INI, SCRIPTS_PATH):
+    print(" 🔍  Verifying required CLI tools")
     os.chdir(SCRIPTS_PATH)
     
     SCRIPTS = os.listdir(SCRIPTS_PATH)
 
-    if SCRIPTS_PATH.endswith("cli"):
-        for script in SCRIPTS:
-            package = script.split("_")[1]
-            if not check_required_cli_commands(package):
-                print(f" ℹ️  The following package: '{package}' package was not found on System")
-                print(f" ℹ️  Installing: {package} package now")
-                run_command(COMMAND=f"ansible-playbook -i {INVENTORY_INI} {script}", shell=True)
-        return "✅ ALL CLI Tools are ok!"
-
-def run_ansible_script(BASE_PATH, SCRIPTS_PATH):
-    os.chdir(SCRIPTS_PATH)
-    SCRIPTS = os.listdir(SCRIPTS_PATH)
-
     for script in SCRIPTS:
-        print(" ℹ️  Applying Ansible scripts")
-        if script == 'get-argocd-credentials.yml':
-            get_argocd_credentials(BASE_PATH)
-        else:
-            print(f" ℹ️  Applying Ansible script: {script}")
-            run_command(COMMAND=f"ansible-playbook {script}", shell=True)
-        print(f" ✅ Script applied!")
-    return "✅ All scripts has been applied in the Environment!"
+        package = script.split("_")[1]
+        if not check_required_cli_commands(package):
+            print(f" ❌  The following package: '{package}' package was not found on System")
+            print(f" ℹ️  Installing: {package} package now")
+            run_command(COMMAND=f"ansible-playbook -i {INVENTORY_INI} {script}", shell=True)
+    print(" ✅  All required CLI Tools are ok!")
+
+def run_general_scripts(INVENTORY_INI, SCRIPTS_PATH):
+    print(" ℹ️  Applying General Ansible scripts")
+    run_command(COMMAND=f"ansible-playbook -i {INVENTORY_INI} {SCRIPTS_PATH}", shell=True)
+    print(f" ✅  Script applied!")
+    return " ✅  All scripts has been applied in the Environment!"
     
 if __name__ == '__main__':
     print("Please, run init.py script located in the Root Directory of this repository")
