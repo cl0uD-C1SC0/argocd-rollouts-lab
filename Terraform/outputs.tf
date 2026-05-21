dadb19bbb1db26536741b77ac37f0cd689620e48
José Silva <90631269+cl0uD-C1SC0@users.noreply.github.com>
2026-05-21 16:56:32 -0300

    chore: removed non used file

--- Terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "codecommit-cred-user" {
-  value     = module.codecommit.codecommit-cred-user
-  sensitive = true
-}
-
-output "codecommit-cred-password" {
-  value     = module.codecommit.codecommit-cred-password
-  sensitive = true
-}
